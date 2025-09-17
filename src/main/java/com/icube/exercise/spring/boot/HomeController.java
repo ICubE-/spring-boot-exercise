ec17aee62ad9466180fd9e92d4a5a86ce2e82403
ICubE- <jehoyeon01@gmail.com>
2025-09-17 17:26:55 +0900

    Add home page

---
+++ src/main/java/com/icube/exercise/spring/boot/HomeController.java
@@ -0,0 +1,12 @@
+package com.icube.exercise.spring.boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
