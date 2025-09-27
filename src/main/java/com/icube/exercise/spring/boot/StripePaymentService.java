ebfb309aeb7f6483841e1230a45b0baf62644803
ICubE- <jehoyeon01@gmail.com>
2025-09-27 15:13:17 +0900

    Annotate StripePaymentService as a primary service

--- src/main/java/com/icube/exercise/spring/boot/StripePaymentService.java
+++ src/main/java/com/icube/exercise/spring/boot/StripePaymentService.java
@@ -1,5 +1,10 @@
 package com.icube.exercise.spring.boot;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
 public class StripePaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
