322d10c933a42158db6575cef21ab4593caf2dfb
ICubE- <jehoyeon01@gmail.com>
2025-10-02 16:09:27 +0900

    Avoid N+1 problem using EntityGraph

--- src/main/java/com/icube/exercise/spring/boot/services/UserService.java
+++ src/main/java/com/icube/exercise/spring/boot/services/UserService.java
@@ -127,4 +127,13 @@ public class UserService {
         var user = userRepository.findByEmail("jeho.yeon@example.org").orElseThrow();
         System.out.println(user);
     }
+
+    @Transactional
+    public void fetchUsers() {
+        var users = userRepository.findAllWithAddresses();
+        users.forEach(u -> {
+            System.out.println(u);
+            u.getAddresses().forEach(System.out::println);
+        });
+    }
 }
