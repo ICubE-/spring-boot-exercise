efabfea1beee01b2e9c3bc9aab392f9e866f2120
ICubE- <jehoyeon01@gmail.com>
2025-10-03 16:58:33 +0900

    Build a basic API

---
+++ src/main/java/com/icube/exercise/spring/boot/entities/Message.java
@@ -0,0 +1,10 @@
+package com.icube.exercise.spring.boot.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+    private String text;
+}
