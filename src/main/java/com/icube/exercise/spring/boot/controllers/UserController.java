88694adc4adc8e75d6d0966d136653b87883648c
ICubE- <jehoyeon01@gmail.com>
2025-10-28 20:31:48 +0900

    Extract query parameters

--- src/main/java/com/icube/exercise/spring/boot/controllers/UserController.java
+++ src/main/java/com/icube/exercise/spring/boot/controllers/UserController.java
@@ -4,11 +4,11 @@ import com.icube.exercise.spring.boot.dtos.UserDto;
 import com.icube.exercise.spring.boot.mappers.UserMapper;
 import com.icube.exercise.spring.boot.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @AllArgsConstructor
 @RestController
@@ -18,8 +18,14 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(
+            @RequestParam(required = false, defaultValue = "", name = "sort") String sort
+    ) {
+        if (!Set.of("name", "email").contains(sort)) {
+            sort = "name";
+        }
+
+        return userRepository.findAll(Sort.by(sort))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
