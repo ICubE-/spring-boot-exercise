6e2168d78ad843774406ea2326a94a3acf020e2b
ICubE- <jehoyeon01@gmail.com>
2025-10-28 19:19:31 +0900

    Create getUser()

--- src/main/java/com/icube/exercise/spring/boot/controllers/UserController.java
+++ src/main/java/com/icube/exercise/spring/boot/controllers/UserController.java
@@ -4,15 +4,23 @@ import com.icube.exercise.spring.boot.entities.User;
 import com.icube.exercise.spring.boot.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
-    public Iterable<User> getAllUsers(){
+    @GetMapping
+    public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
